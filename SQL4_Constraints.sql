57ac164c1c66d9f413b4fa1678678177b8b025aa
Douglas Sabwa Indumwa <dglsdeveloper@gmail.com>
2025-02-12 23:18:06 +0300

    Feat: Drop movies table and recreate it with constraints

--- SQL4_Constraints.sql
+++ SQL4_Constraints.sql
@@ -1,10 +1,19 @@
--- Step 3: Create Movies Table
+-- Step 3: Movies Table Constraints
 
 CREATE TABLE movies (
-    movie_id INT,
-    title VARCHAR(255),
-    release_year YEAR,
-    category VARCHAR(100),
-    views BIGINT
+    movie_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    release_year YEAR NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    views BIGINT NOT NULL
+);
+
+-- Step 4: Actors Table Constraints
+
+CREATE TABLE actors (
+    actor_id INT AUTO_INCREMENT PRIMARY KEY,
+    actor_name VARCHAR(255) NOT NULL,
+    birth_date DATE,
+    country VARCHAR(100),    
 );
 
